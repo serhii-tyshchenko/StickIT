7d43e8fd749e01b2afc633b1cee7f2df5f14afd5
Serhii Tyshchenko <sehii.tyshchenko84@gmail.com>
2022-06-03 19:24:31 +0300

    added render tests

---
+++ src/components/IconButton/IconButton.spec.js
@@ -0,0 +1,14 @@
+import renderer from 'react-test-renderer';
+
+import { IconButton } from './index';
+
+describe('IconButton', () => {
+  it('renders correctly', () => {
+    const props = {
+      onClick: jest.fn(),
+      icon: 'plus'
+    };
+    const component = renderer.create(<IconButton {...props} />).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
