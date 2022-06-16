b454ee03609c37d0e307c858010ea06f29f74085
Serhii Tyshchenko <sehii.tyshchenko84@gmail.com>
2022-06-16 21:33:50 +0300

    added new sticker styles

--- src/components/StickerList/StickerListItem/StickerListItem.jsx
+++ src/components/StickerList/StickerListItem/StickerListItem.jsx
@@ -31,52 +31,48 @@ function StickerListItem({ sticker }) {
   } = useSticker(sticker);
 
   return (
-    <li className={NAME_SPACE}>
-      <div className={`${NAME_SPACE}__container`} style={{ backgroundColor: color }}>
-        <div className={`${NAME_SPACE}__header`}>
-          <input
-            className={`${NAME_SPACE}__title`}
-            type="text"
-            value={state.title}
-            name="title"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            placeholder={dic.stickerTitlePlaceholder}
+    <li className={NAME_SPACE} style={{ backgroundColor: color }}>
+      <header className={`${NAME_SPACE}__header`}>
+        <input
+          className={`${NAME_SPACE}__title`}
+          type="text"
+          value={state.title}
+          name="title"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder={dic.stickerTitlePlaceholder}
+        />
+        <div className={`${NAME_SPACE}__controls`}>
+          <IconButton
+            extraClassName={`${NAME_SPACE}__btn`}
+            icon="color-adjust"
+            onClick={toggleColorPicker}
+            title={dic.changeColorAlt}
           />
-          <div className={`${NAME_SPACE}__controls`}>
-            <IconButton
-              extraClassName={`${NAME_SPACE}__btn`}
-              icon="color-adjust"
-              onClick={toggleColorPicker}
-              title={dic.changeColorAlt}
-            />
-            <IconButton
-              extraClassName={`${NAME_SPACE}__btn`}
-              icon={isPinned ? 'pin' : 'pin-outline'}
-              onClick={handlePinStickerClick}
-              title={dic.pinStickerAlt}
-            />
-            <IconButton
-              extraClassName={`${NAME_SPACE}__btn`}
-              icon="trash-empty"
-              onClick={handleRemoveStickerClick}
-              title={dic.removeStickerAlt}
-            />
-          </div>
-          <ColorPicker opened={isColorPickerOpen} color={color} onChange={handleColorChange} />
-        </div>
-        <div className={`${NAME_SPACE}__details`}>
-          <textarea
-            className={`${NAME_SPACE}__text`}
-            name="text"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            type="textarea"
-            value={state.text}
-            placeholder={dic.stickerTextPlaceholder}
+          <IconButton
+            extraClassName={`${NAME_SPACE}__btn`}
+            icon={isPinned ? 'pin' : 'pin-outline'}
+            onClick={handlePinStickerClick}
+            title={dic.pinStickerAlt}
+          />
+          <IconButton
+            extraClassName={`${NAME_SPACE}__btn`}
+            icon="trash-empty"
+            onClick={handleRemoveStickerClick}
+            title={dic.removeStickerAlt}
           />
         </div>
-      </div>
+        <ColorPicker opened={isColorPickerOpen} color={color} onChange={handleColorChange} />
+      </header>
+      <textarea
+        className={`${NAME_SPACE}__text`}
+        name="text"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        type="textarea"
+        value={state.text}
+        placeholder={dic.stickerTextPlaceholder}
+      />
     </li>
   );
 }
